447d20b4a6bda3253cd3d2c72e804219f661d4cc
luca minnei <62435198+minn3@users.noreply.github.com>
2022-05-25 15:33:51 +0200

    testing functions

--- Python/ProgettoIsw.py/main.py
+++ Python/ProgettoIsw.py/main.py
@@ -1,13 +1,14 @@
-import sys
 import string
-
+import sys
+from test_text_analyzer.TestTextAnalyzer import *
 
 class TextAnalyzer:
 
+
     def __init__(self, text_file_name, stopword_file_name, limit_frequency):
         self.punctuation = {char: ' ' for char in list(string.punctuation)}
-        self.text = TextAnalyzer._list_of_words_from_file(self, text_file_name)
-        self.stopwords = list(set(TextAnalyzer._list_of_words_from_file(self, stopword_file_name)))
+        self.text = TextAnalyzer.list_of_words_from_file(self, text_file_name)
+        self.stopwords = list(set(TextAnalyzer.list_of_words_from_file(self, stopword_file_name)))
         self.limit_frequency = int(limit_frequency)
 
     def save_frequency_into_file(self, output_file_name, frequency_dictionary):
@@ -15,7 +16,7 @@ class TextAnalyzer:
             for item in frequency_dictionary.items():
                 output_file.write(str(item[0]) + ',' + str(item[1]) + '\n')
 
-    def text_fequency_of_words(self):
+    def text_frequency_of_words(self):
         frequency_dictionary = {}
         for word in self.text:
             if word in frequency_dictionary:
@@ -26,19 +27,19 @@ class TextAnalyzer:
         return dict(sorted(frequency_dictionary.items(), key=return_values_from_item, reverse=True))
 
     def prepare_text_for_analysis_from_file(self):
-        TextAnalyzer._remove_stopwords(self)
+        TextAnalyzer.remove_stopwords(self)
 
-    def _remove_stopwords(self):
+    def remove_stopwords(self):
         self.text = list(filter(None, [word for word in self.text if word not in self.stopwords]))
 
-    def _remove_punctuation_from_line(self, line):
+    def remove_punctuation_from_line(self, line):
         return line.translate(str.maketrans(self.punctuation))
 
-    def _list_of_words_from_file(self, file_name):
+    def list_of_words_from_file(self, file_name):
         list_of_stopwords = []
         with open(file_name) as file:
             for line in file:
-                line_without_puntactions = TextAnalyzer._remove_punctuation_from_line(self, line)
+                line_without_puntactions = TextAnalyzer.remove_punctuation_from_line(self, line)
                 list_of_stopwords.extend(line_without_puntactions.strip().lower().split(' '))
         return list_of_stopwords
 
@@ -60,11 +61,14 @@ class TextAnalyzer:
         return {key: value for key, value in dict_composite_words.items() if value >= self.limit_frequency}
 
 
+
 if __name__ == '__main__':
+    #testing command
+    #unittest.main()
     text, stopwords, input_frequency = sys.argv[1:]
     text_analyzer = TextAnalyzer(text, stopwords, input_frequency)
     text_analyzer.prepare_text_for_analysis_from_file()
-    frequency_analysis_of_words = text_analyzer.text_fequency_of_words()
+    frequency_analysis_of_words = text_analyzer.text_frequency_of_words()
     text_analyzer.save_frequency_into_file('output.txt', frequency_analysis_of_words)
     frequency_analysis_of_composite_words = text_analyzer.text_frequency_of_composite_words()
     text_analyzer.save_frequency_into_file('compositeOutput.txt', frequency_analysis_of_composite_words)

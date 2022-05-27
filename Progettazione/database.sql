13970f137dd3b6c38cc33daa917bf6f845c2c6a8
tvs111 <tvs034@gmail.com>
2022-05-27 13:09:20 +0200

    aggiornato database, classi e chede

--- Progettazione/database.sql
+++ Progettazione/database.sql
@@ -5,21 +5,21 @@
 
 CREATE TABLE Modello(
 	tipo VARCHAR(24) PRIMARY KEY, 
-	marca VARCHAR(24),
-	modello VARCHAR(24),
-	costoOrario DECIMAL(2, 2),
-	costoGiornaliero DECIMAL(2, 2)
+	marca VARCHAR(24) NOT NULL,
+	tipologiaModello VARCHAR(24) NOT NULL,
+	costoOrario DECIMAL(2, 2) NOT NULL,
+	costoGiornaliero DECIMAL(2, 2) NOT NULL
 );
 
 CREATE TABLE Mezzo(
 	numeroMatricola CHAR(7) PRIMARY KEY, 
-	modello VARCHAR(24) REFERENCES Modello,
+	modello VARCHAR(24) NOT NULL REFERENCES Modello,
 	tipologia VARCHAR(24) CHECK (tipologia IN ('bicicletta', 'bicicletta elettrica', 'scoter elettrico',
 									 'scooter a benzina', 'monopattino elettrico')),
-	stato VARCHAR(24) CHECK (stato IN ('disponibile', 'prenotato', 'noleggiato',
+	stato VARCHAR(24) NOT NULL CHECK (stato IN ('disponibile', 'prenotato', 'noleggiato',
 									 'guasto', 'inOperazione', 'radiato')),
-	kmPercorsi DECIMAL(5,2),
-	targa VARCHAR(12) UNIQUE,
+	kmPercorsi DECIMAL(5,2) NOT NULL,
+	targa VARCHAR(12) UNIQUE ,
 	capacitaSerbatoio DECIMAL(2,2),
 	litriPresenti DECIMAL(2),
 	caricaBatterie SMALLINT
@@ -29,7 +29,9 @@ CREATE TABLE Mezzo(
 -- fatto se ha la password settata o meno
 CREATE TABLE Utente(
 	CF CHAR(16) PRIMARY KEY,
-	tipo VARCHAR(24) CHECK (tipo IN ('amministratore', 'operatore', 'cliente')),
+	nome VARCHAR(16) NOT NULL,
+	cognome VARCHAR(16) NOT NULL,
+	tipo VARCHAR(24) NOT NULL CHECK (tipo IN ('amministratore', 'operatore', 'cliente')),
 	password VARCHAR(32),
 	mail VARCHAR(24),
 	telefono CHAR(10),
@@ -45,28 +47,29 @@ CREATE TABLE Utente(
 CREATE TABLE Prenotazione(
 	CFCliente CHAR(16) REFERENCES Utente,
 	nrMatricolaMezzo CHAR(7) REFERENCES Mezzo,
-	dataPrenotazione DATE, 
+	dataPrenotazione DATE NOT NULL, 
 	dataRitiro TIMESTAMP,
-	dataConsegnaPrevista TIMESTAMP,
+	dataConsegnaPrevista TIMESTAMP NOT NULL,
 	dataConsegnaEffettiva TIMESTAMP,
 	PRIMARY KEY(CFCliente, nrMatricolaMezzo, dataRitiro)
 );
 
 CREATE TABLE Operazione(
 	idOperazione SERIAL PRIMARY KEY,
-	nrMatricolaMezzo CHAR(7) REFERENCES Mezzo,
-	dataInizio DATE, 
+	nrMatricolaMezzo CHAR(7) NOT NULL REFERENCES Mezzo,
+	dataInizio DATE NOT NULL, 
 	dataFine TIMESTAMP,
 	tipoIntervento VARCHAR(50) CHECK (tipoIntervento in ('manutenzione', 'riparazione')),
 	descizioneOperazione VARCHAR(100),
 	costo DECIMAL (4,2),
-	officina VARCHAR(24)
+	officina VARCHAR(24) NOT NULL
 );
 
 CREATE TABLE Attesa(
 	idAttesa SERIAL PRIMARY KEY,
-	CFCliente CHAR(16) REFERENCES Utente,
-	modelloMezzo VARCHAR(24) REFERENCES Modello,
-	dataInizioAttesa TIMESTAMP, 
-	dataRitiroPrevista TIMESTAMP
+	CFCliente CHAR(16) NOT NULL REFERENCES Utente,
+	modelloMezzo VARCHAR(24) NOT NULL REFERENCES Modello,
+	dataInizioAttesa TIMESTAMP NOT NULL, 
+	dataRitiroPrevista TIMESTAMP NOT NULL,
+	dataConsegnaPrevista TIMESTAMP NOT NULL
 );

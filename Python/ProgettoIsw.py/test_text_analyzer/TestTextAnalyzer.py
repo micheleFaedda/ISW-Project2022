0da17d6de2c04949cef7d7f7a59652d7984a8bfe
luca minnei <62435198+minn3@users.noreply.github.com>
2022-06-05 09:33:02 +0200

    setUp nel testing

--- Python/ProgettoIsw.py/test_text_analyzer/TestTextAnalyzer.py
+++ Python/ProgettoIsw.py/test_text_analyzer/TestTextAnalyzer.py
@@ -3,8 +3,8 @@ import unittest
 from main import TextAnalyzer
 
 class TestTextAnalyzer(unittest.TestCase):
-
-    text_analyzer = TextAnalyzer('test_output.txt', 'test_output.txt', 1)
+    def setUp(self):
+        self.text_analyzer = TextAnalyzer('test_output.txt', 'test_output.txt', 1)
 
     def test_text_analyzer_return_exception_file_not_found(self):
         self.assertRaises(FileNotFoundError, TextAnalyzer.list_of_words_from_file,self.text_analyzer, 'pippo.txt')
